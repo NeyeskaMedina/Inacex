e24afcfa9689dd6c7a5ed08d38af63cf36553c9a
Neyeska Medina <neyeska.go@gmail.com>
2025-07-31 23:28:29 -0400

    Test base64

--- server/src/api/v1/controllers/login/postLogin.js
+++ server/src/api/v1/controllers/login/postLogin.js
@@ -1,24 +1,29 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+const adminHash = Buffer.from(process.env.ADMIN_PASS_HASH, 'base64').toString('utf-8');
+const ejecHash = Buffer.from(process.env.EJEC_PASS_HASH, 'base64').toString('utf-8');
+
+
 const postLogin = async (req, res) => {
   const { username, password } = req.body;
   console.log(username, password);
 
+
   let validUser = null;
-  let storedHash = Buffer.from(process.env.ADMIN_PASS_HASH, 'base64').toString('utf-8');
+  let storedHash = null
   let role = null;
 
   if (username === process.env.ADMIN_USER) {
     console.log(process.env.ADMIN_USER);
     
     validUser = process.env.ADMIN_USER;
-    storedHash = process.env.ADMIN_PASS_HASH;
+    storedHash = adminHash;
     role = 'admin';
     
   } else if (username === process.env.EJEC_USER) {
     validUser = process.env.EJEC_USER;
-    storedHash = process.env.EJEC_PASS_HASH;
+    storedHash = ejecHash;
     role = 'ejecutiva';
   } else {
     return res.status(401).json({ message: 'Usuario no encontrado' });
